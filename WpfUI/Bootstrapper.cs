03e16f055c28f56c28af4549e2619c67e8b64c6b
Van Stonecypher <van.stonecypher@quantitech.com>
2018-06-21 08:39:13 -0500

    Added duplicate application check.

--- WpfUI/Bootstrapper.cs
+++ WpfUI/Bootstrapper.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using WpfUI.ViewModels;
@@ -18,7 +20,18 @@ namespace WpfUI
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
-            DisplayRootViewFor<ShellViewModel>();
+            string mutexName = Assembly.GetExecutingAssembly().GetType().GUID.ToString();
+            using (Mutex mutex = new Mutex(true, mutexName, out bool createdNew))
+            {
+                if (createdNew)
+                {
+                    DisplayRootViewFor<ShellViewModel>();
+                }
+                else // Only allow one copy of application to run
+                {
+                    DisplayRootViewFor<DuplicateViewModel>(); 
+                }
+            }
         }
     }
 }
---
+++ WpfUI/ViewModels/DuplicateViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUI.ViewModels
+{
+    public class DuplicateViewModel : Screen
+    {
+        public string ViewTitle
+        {
+            get { return _viewTitle; }
+            set
+            {
+                if (value != _viewTitle)
+                {
+                    _viewTitle = value;
+                    NotifyOfPropertyChange();
+                }
+            }
+        }
+        private string _viewTitle = "Kill Ring";
+
+        
+
+        public string ExplanationText
+        {
+            get { return _explanationText; }
+            set
+            {
+                if (value != _viewTitle)
+                {
+                    _explanationText = value;
+                    NotifyOfPropertyChange();
+                }
+            }
+        }
+        private string _explanationText = "Another version of Kill Ring detected and only one is allowed to run at a time. Click Exit or close this window to exit the application.";
+
+
+        public void Exit()
+        {
+            TryClose();
+        }
+    }
+}
